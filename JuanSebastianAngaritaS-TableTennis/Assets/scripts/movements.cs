e79326935620e3d6a548dda124c608db4446836e
DESKTOP-2CT8KTQ\Sebas <jsas1996@gmail.com>
2018-10-09 22:03:34 -0500

    Aquí la entrega

---
+++ JuanSebastianAngaritaS-TableTennis/Assets/scripts/movements.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movements : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.forward * 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.back * 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.up * 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.down * 0.1f);
+        }
+    }
+}
